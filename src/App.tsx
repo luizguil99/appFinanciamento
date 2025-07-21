952d8e8d4aec467b3a5aea1e7f75d0c226559e9d
luizguil99 <124329344+luizguil99@users.noreply.github.com>
2025-07-21 18:00:08 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Vite/Typescript-Template</h1>
+    </div>
+  );
+}
+
+export default App;
